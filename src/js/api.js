db3cff4bb4e317d4cf6a5bb15cb766d71d3d6ab6
Kim Anh Do <kim@Kims-MBP.fritz.box>
2023-10-30 13:50:58 +0100

    ok

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-export const unsplashAccessKey = UNSPLASH_ACCESS_KEY;
+export const unsplashAccessKey = "P4q-HQvojhih476NMAOCRW4FgYoP7xNjyoiU9PsgMP0";
 
 export async function getApi(url) {
   let fetchResponse = await fetch(url);
